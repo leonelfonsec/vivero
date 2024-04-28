0ae5764b7d0b6a10a9bf740c197179c696ed071a
Leonel Fonseca <n.fonsecao@uniandes.edu.co>
2024-04-28 15:45:12 -0500

    Crecion de clase con los atributos de planta

--- src/app/plantas/planta.ts
+++ src/app/plantas/planta.ts
@@ -1,24 +1,18 @@
 export class Planta {
  id: number;
- name: string;
- isbn: string;
- description: string;
- image: string;
- publishingDate: any;
+ nombre_comun: string;
+ tipo: string;
+ clima: string;
 
  constructor(
    id: number,
-   name: string,
-   isbn: string,
-   description: string,
-   image: string,
-   publishingDate: any
+   nombre_comun: string,
+   tipo: string,
+   clima: string
  ) {
    this.id = id;
-   this.name = name;
-   this.isbn = isbn;
-   this.description = description;
-   this.image = image;
-   this.publishingDate = publishingDate;
+   this.nombre_comun = nombre_comun;
+   this.tipo = tipo;
+   this.clima = clima;
  }
 }
