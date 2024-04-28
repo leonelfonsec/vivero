d942d829638710d8fba5c1d4d8d475166ba6df89
Leonel Fonseca <n.fonsecao@uniandes.edu.co>
2024-04-28 16:01:18 -0500

    Terminar de ajustar el componente

--- src/app/plantas/planta.service.ts
+++ src/app/plantas/planta.service.ts
@@ -1,11 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { Planta } from './planta';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlantaService {
 
+  private apiUrl: string = environment.baseUrl;
+
 constructor(private http: HttpClient) { }
+
+getPlantas(): Observable<Planta[]> {
+  return this.http.get<Planta[]>(this.apiUrl);
+}
 
 }
